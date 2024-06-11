365c6b5134633b5026925f6a6064dc535f6b58d7
ColinTimBarndt <colin.barndt@pm.me>
2024-06-11 23:05:51 +0200

    Remove wee_alloc dependency due to memory leak

--- src/rs/lib.rs
+++ src/rs/lib.rs
@@ -1,13 +1,8 @@
-extern crate wee_alloc;
-
 use libflate::gzip::{Decoder, Encoder};
-use std::alloc::{GlobalAlloc, Layout};
+use std::alloc::Layout;
 use std::io::{Read, Write};
 use std::ptr::slice_from_raw_parts;
 use wasm_bindgen::prelude::wasm_bindgen;
-
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 static mut BUFFER: Vec<u8> = Vec::new();
 static mut ERROR_MESSAGE: String = String::new();
@@ -35,12 +30,12 @@ pub unsafe fn error_message_len() -> usize {
 
 #[wasm_bindgen]
 pub unsafe fn malloc_u8(size: usize) -> *mut u8 {
-	ALLOC.alloc(Layout::array::<u8>(size).unwrap())
+	std::alloc::alloc(Layout::array::<u8>(size).unwrap())
 }
 
 #[wasm_bindgen]
 pub unsafe fn free_u8(ptr: *mut u8, size: usize) {
-	ALLOC.dealloc(ptr, Layout::array::<u8>(size).unwrap());
+	std::alloc::dealloc(ptr, Layout::array::<u8>(size).unwrap());
 }
 
 /// Deallocates the buffer to free memory
